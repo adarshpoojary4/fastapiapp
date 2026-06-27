4df6cc9f0da461af8c92aecf8d25b7d406e0df6a
Adarsh <adarshbillava243@gmail.com>
2026-06-27 14:14:02 +0530

    git test

--- routers/job.py
+++ routers/job.py
@@ -1,16 +1,39 @@
+
 from fastapi import APIRouter
+from schemas import job
+from schemas.job import JobCreate, JobUpdate
 
-Router = APIRouter(prefix="/job", tags=["job"])
+router = APIRouter(prefix="/job", tags=["job"])
+jobs = []
 
+@router.post("/")
+def create_job(job: JobCreate):
+    jobs.append(job)
+    return jobs
 
 @router.get("/")
-def read_root():
-    return {"job":"job root"}
+def get_all_job():
+    return jobs
 
 @router.get("/{job_id}")
-def read_job():
-    return {"job": "job root"}
+def get_job(job_id: int):
+    return jobs[job_id]
 
-@router.get("/{job_id}")
-def read_job(job_id: int):
-    return {"job_id": job_id}
+@router.put("/{job_id}")
+def update_job(job_id: int, job: JobUpdate):
+    jobs[job_id] = job
+    return jobs
+
+@router.delete("/{job_id}")
+def delete_job(job_id: int):    
+    jobs.pop(job_id)
+    return jobs
+
+
+# @router.get("/")
+# def read_job():
+#     return {"job": "Job root"}
+
+# @router.get("/{job_id}")
+# def read_job(job_id: int):
+#     return {"job_id": job_id}
