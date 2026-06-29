c77c0cd37dc206cc635f2b4f48d50295f1bfab97
Adarsh <adarshbillava243@gmail.com>
2026-06-29 10:27:19 +0530

    git test

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Akrithi%40123@localhost:5432/student_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Adarsh%402438@localhost:5433/student_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
