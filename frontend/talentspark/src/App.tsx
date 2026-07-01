fcfee9c2e14222004dee8204f5d3f79ec5d5b762
Adarsh <adarshbillava243@gmail.com>
2026-07-01 12:39:02 +0530

    g

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,23 +1,22 @@
-import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+// import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
-import NotFound from "./components/NotFound";
-import { getCompanies } from "./Services/CompanyService";
-import type { Company } from "./types/company";
+import Footer from "./components/Footer";
+import {useEffect,useState} from "react";
+import { getCompanies,updateCompany,deleteCompany,createCompany } from "./Services/CompanyService";
+import type {Company} from "./types/company"
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-  const [companies, setCompanies] = useState<Company[]>([]);
+function App(){
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState<Error | null>(null)
+  const [companies,setCompanies] = useState<Company[]>([]);
 
   async function fetchCompanies() {
     setLoading(true);
     try {
-      const companies = await getCompanies();
-      setCompanies(companies);
+      const company = await getCompanies();
+      setCompanies(company);
     } catch (err) {
       setError(err as Error);
     } finally {
@@ -25,34 +24,61 @@ function App() {
     }
   }
 
+  async function handleEdit(company:Company){
+    try{
+      const updatedCompany = await updateCompany(company.id,company);
+      setCompanies(companies.map((company) => company.id === updatedCompany.id ? updatedCompany : company));
+    }catch(err){
+      setError(err as Error);
+    }
+  }
+
+  async function handleDelete(id:number){
+    try{
+      await deleteCompany(id);
+      setCompanies(companies.filter((company) => company.id !== id));
+    }catch(err){
+      setError(err as Error);
+    }
+  }
+
+  async function handleAdd(company:Company){
+    try{
+      const newCompany = await createCompany(company);
+      setCompanies([...companies,newCompany]);
+    }catch(err){
+      setError(err as Error);
+    }
+  }
+
+
   useEffect(() => {
     fetchCompanies();
   }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
+  
+  if(loading){
+    return <div>Loading...</div>
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if(error){
+    return <div>Error: {error.message}</div>
   }
-
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <NavBar />
-            <CompanyCard companies={companies} />
-            <JobCard />
-            <Footer />
-          </>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+  
+  return(
+    <>
+    <NavBar />
+    {/* <Welcome /> */}
+    <br />
+    <CompanyCard 
+    companies={companies}
+    onedit={handleEdit}
+    ondelete={handleDelete}
+    onadd={handleAdd}
+    />
+    <JobCard />
+    <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
